b169fed31cc8e061467d576b86ed6b87f40b6ee8
Sharjeel-22 <pcraboot55441@gmail.com>
2023-07-08 03:05:00 +0500

    code setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:["https://www.google.com/"],
+        formats:['image/webp']
+      }
+}
 
 module.exports = nextConfig
